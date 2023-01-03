94a16789d9417afcfdb4e96b6982a9e4b7e2f9b9
rash <orashusedmund@gmail.com>
2023-01-03 05:16:52 +0100

    .

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,8 +1,14 @@
 const menuIcon = document.querySelector('.menu-icon')
 const varyingList = document.querySelector('.varying-list')
+const listItems = document.querySelectorAll('.list-items')
 console.log('code works')
+console.log(listItems)
 
 menuIcon.addEventListener('click', () => {
-  console.log('code works')
   varyingList.classList.toggle('active-menu')
+  listItems.forEach((item) => {
+    item.addEventListener('click', () => {
+      varyingList.classList.remove('active-menu')
+    })
+  })
 })
